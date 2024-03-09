92ca7ffc80a5e5b842f0986d87c866a612759c91
amiranazri <amira@lifelinelab.io>
2024-03-09 09:53:30 +0800

    Merged with main branch

---
+++ .history/app/layout_20240309095222.tsx
@@ -0,0 +1,41 @@
+import "@/app/ui/global.css";
+import { open_sans } from "@/app/ui/fonts";
+import Navbar from "@/app/ui/components/Navbar";
+import Footer from "@/app/ui/components/Footer";
+import Script from "next/script";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Brightminds School of Learning</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Madimi+One&display=swap')
+        </style>
+      </head>
+      <body className={`${open_sans.className} antialiased`}>
+        <ToastContainer position="top-center" />
+        <div className="mt-[67px] md:mt-[115px]">
+          <Navbar />
+          <div className="min-h-screen">{children}</div>
+          <Footer />
+        </div>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/flowbite@2.3.0/dist/flowbite.min.js"
+          strategy="beforeInteractive"
+        />
+      </body>
+    </html>
+  );
+}
