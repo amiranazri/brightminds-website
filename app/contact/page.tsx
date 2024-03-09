b75a8eacf7794d8a87d3a6ad6102da2da9706f30
amiranazri <amira@lifelinelab.io>
2024-03-09 11:27:43 +0800

    testing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -19,8 +19,16 @@ function ContactUs() {
         <div className="text-primary-blue text-2xl font-bold mt-3 mb-3">
           How can we help?
         </div>
+        <div>
+          Your satisfaction is our top priority. Whether you have a question
+          about our services, need assistance with an order, or simply want to
+          share your feedback, we are here to help. Please feel free to reach
+          out using the form below, and our dedicated team will get back to you
+          as soon as possible. Thank you for choosing us - we look forward to
+          assisting you!
+        </div>
 
-        <div className="flex flex-col items-center space-y-3 w-[100%] lg:items-start mt-5">
+        {/* <div className="flex flex-col items-center space-y-3 w-[100%] lg:items-start mt-5">
           <ContactInfo
             imgSrc="/brightminds_logo_1.png"
             alt="Marker"
@@ -39,7 +47,7 @@ function ContactUs() {
             title="PHONE"
             subTitle="(+60) 111-232-1134"
           />
-        </div>
+        </div> */}
       </div>
       {/* Form Part */}
       <div className="w-[300px] sm:w-[600px] lg:w-[500px] lg:mr-10 mt-5 lg:mt-0 flex flex-col items-center relative">
