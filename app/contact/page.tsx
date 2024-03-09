4f19d892a42f8159144cd3818920ae04bd377169
amiranazri <amira@lifelinelab.io>
2024-03-09 12:06:54 +0800

    testing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,20 +13,19 @@ function ContactUs() {
 
   return (
     <div className="max-w-[1920px] text-primary-black m-5 mt-[170px] lg:mt-[300px] flex flex-col items-center justify-center lg:flex-row-reverse">
-      {/* Text Part */}
       <div className="max-w-lg flex flex-col items-center lg:ml-10">
-        {/* <div className="text-primary-yellow font-bold">CONTACT US</div> */}
-        {/* <div className="text-primary-blue text-2xl font-bold mt-3 mb-3">
+        <div className="text-primary-yellow font-bold">CONTACT US</div>
+        <div className="text-primary-blue text-2xl font-bold mt-3 mb-3">
           How can we help?
-        </div> */}
-        {/* <div>
+        </div>
+        <div>
           Your satisfaction is our top priority. Whether you have a question
           about our services, need assistance with an order, or simply want to
           share your feedback, we are here to help. Please feel free to reach
           out using the form below, and our dedicated team will get back to you
           as soon as possible. Thank you for choosing us - we look forward to
           assisting you!
-        </div> */}
+        </div>
 
         <div className="flex flex-col items-center space-y-3 w-[100%] lg:items-start mt-5">
           <ContactInfo
@@ -49,7 +48,6 @@ function ContactUs() {
           />
         </div>
       </div>
-      {/* Form Part */}
       <div className="w-[300px] sm:w-[600px] lg:w-[500px] lg:mr-10 mt-5 lg:mt-0 flex flex-col items-center relative">
         <FormDropDown setState={setSelectedForm} />
         <div
