cee527a48602c0b50fce16367049ecd8eda1810b
Kathiressan Sivanes <k.washroom.space@gmail.com>
2024-03-11 09:15:22 +0800

    initial portfolio card

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import PortfolioCard from "./PortfolioCard";
+import Title from "@/app/ui/components/Title"
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div className="max-w-[1920px]">
+      {/* Portfolio Cards*/}
+      <div className='mt-[113px] md:mt-[136px] min-[918px]:mt-[-2px]'>
+        <Title title='PROJECT PORTFOLIO' />
+        <PortfolioCard />
+      </div>
+    </div>
   )
 }
 
