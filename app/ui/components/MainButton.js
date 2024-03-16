766917be0a02eba6bb73a99ac175f471b02f47e0
amiranazri <amira@lifelinelab.io>
2024-03-16 20:02:27 +0800

    updated button on corporate page

--- app/ui/components/MainButton.js
+++ app/ui/components/MainButton.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import styled from 'styled-components';
-import Link from 'next/link';
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
 
-function MainButton({text="PARTNER WITH US"}) {
+function MainButton({ text = "Partner With Us" }) {
   return (
-    <div className="text-center bg-primary-blue w-[200px] text-white mx-auto p-2 rounded-md cursor-pointer hover:opacity-80 focus:scale-95" style={{transition: "opacity 0.5s ease"}}>
-        <Link href="contact/corporate">{text}</Link>
+    <div
+      className="text-center mx-auto mb-16 bg-[#19598F] hover:bg-yellow-300 text-white hover:text-black font-bold py-2 px-4 rounded-full lg:w-[170px]"
+      style={{ transition: "opacity 0.5s ease" }}
+    >
+      <Link href="contact/corporate">{text}</Link>
     </div>
-  )
+  );
 }
 
-export default MainButton
+export default MainButton;
