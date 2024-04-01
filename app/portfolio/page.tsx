bce3111aebba280886eced6aa911dcb2a8afcb7c
amiranazri <amira@lifelinelab.io>
2024-04-01 22:22:32 +0800

    Fixed oversized margin portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -8,7 +8,7 @@ import PortfolioData from "./portfolioData.json";
 function Portfolio() {
   return (
     <div className="max-w-[1920px] mx-auto my-24">
-      <div className="mt-[170px] lg:mt-[200px]">
+      <div className="mt-24 md:mt-16">
         <h1 className="mb-16">Project Portfolio</h1>
 
         <div className="flex flex-wrap items-center justify-center">
