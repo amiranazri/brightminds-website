e0015f6ffef9993b246f7e8b61149260dadad1fc
amiranazri <amira@lifelinelab.io>
2024-06-02 10:39:11 +0800

    updated carousel

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -6,6 +6,7 @@ import BlogCard from "../ui/components/BlogCard";
 import BlogData from "../../data/blog-data.json";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import styles from "./events.module.css";
 
 function Events() {
   const router = useRouter();
@@ -20,57 +21,25 @@ function Events() {
 
   const carouselImages = [
     {
-      image: (
-        <Image
-          src="/coding_banner.jpg"
-          width={140}
-          height={140}
-          layout="responsive"
-          alt="bright minds"
-        />
-      ),
+      image: "/coding_banner.jpg",
       title: "Coding for Kids Workshop",
       subtitle:
         "Join our Coding for Kids Workshop and introduce your child to the exciting world of programming with our experienced instructor there to guide.",
     },
     {
-      image: (
-        <Image
-          src="/critical_thinking_banner.jpg"
-          width={140}
-          height={140}
-          layout="responsive"
-          alt="photo1"
-        />
-      ),
+      image: "/critical_thinking_banner.jpg",
       title: "Critical Thinking Workshop",
       subtitle:
         "Our Critical Analysis course equips students with the essential skills to think critically and analyze information effectively through engaging activities and discussions.",
     },
     {
-      image: (
-        <Image
-          src="/debate_club.jpg"
-          width={140}
-          height={140}
-          layout="responsive"
-          alt="photo2"
-        />
-      ),
+      image: "/debate_club.jpg",
       title: "Debate Club",
       subtitle:
         "Our Debate club cultivates students' critical thinking, research, and argumentation skills through structured debates on relevant topics.",
     },
     {
-      image: (
-        <Image
-          src="/financial_literacy_banner.jpg"
-          width={140}
-          height={140}
-          layout="responsive"
-          alt="photo3"
-        />
-      ),
+      image: "/financial_literacy_banner.jpg",
       title: "Financial Literacy Workshop",
       subtitle:
         "In our Financial Literacy course, students develop a foundational understanding of financial concepts such as budgeting, saving, investing, and managing debt.",
@@ -110,25 +79,33 @@ function Events() {
         customTransition="transform 300ms ease-in-out"
         transitionDuration={500}
         removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
-        containerClass="carousel-container"
+        containerClass={`carousel-container ${styles.carouselContainer}`}
         className="sm:mt-16 mt-16"
       >
         {carouselImages.map((item, index) => (
-          <div key={index} className="image-container">
-            {item.image}
-            <div className="text-container">
-              {/* <h1 className={`text-white text-left responsive-text`}>
+          <div key={index} className={styles.carouselItem}>
+            <div className={styles.textColumn}>
+              <h1 className="lg:px-8 md:px-4 sm:px-4 text-left responsive-text">
                 {item.title}
               </h1>
-              <p className={`lg:px-40 md:px-4 sm:px-4 text-white text-left`}>
+              <p className="lg:px-8 md:px-4 sm:px-4 text-left">
                 {item.subtitle}
-              </p> */}
+              </p>
               <button
                 onClick={handleLearnMore}
-                className="my-8 bg-[#19598F] hover:bg-yellow-300 text-white hover:text-black font-bold py-2 px-4 rounded-full lg:w-[170px]"
+                className="my-8 mx-8 bg-[#19598F] hover:bg-yellow-300 text-white hover:text-black font-bold py-2 px-4 rounded-full lg:w-[170px]"
               >
                 <h6>Learn More</h6>
               </button>
+            </div>
+            <div className={styles.imageColumn}>
+              <Image
+                src={item.image}
+                layout="responsive"
+                width={700}
+                height={475}
+                alt={item.title}
+              />
             </div>
           </div>
         ))}
