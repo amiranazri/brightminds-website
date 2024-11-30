cfab162a9a514d4e65d573fb0ef841be624ede2e
Kathiressan Sivanes <kathiressansivanes26@gmail.com>
2024-11-30 11:54:45 +0800

    make phone number optional in all forms

--- app/contact/RegistrationForm.js
+++ app/contact/RegistrationForm.js
@@ -172,13 +172,12 @@ function RegistrationForm({ isFree = false }) {
             id="floating_phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="floating_phone"
             className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Phone number (+60123456789) <span className="text-red-500">*</span>
+            Phone number (+60123456789)
           </label>
         </div>
         <div className="relative z-0 w-full mb-5 group">
--- app/contact/SchoolForm.js
+++ app/contact/SchoolForm.js
@@ -15,15 +15,15 @@ function RegistrationForm() {
   //   "Public Speaking",
   // ]);
 
-    const [subjects, setSubjects] = useState([
-      "Communication Skills",
-      "Financial Literacy",
-      "Public Speaking",
-      "Critical Analysis",
-      "Entrepreneurial Skills",
-      "Debate",
-      "Leadership Skills",
-      "Writing Skills",
+  const [subjects, setSubjects] = useState([
+    "Communication Skills",
+    "Financial Literacy",
+    "Public Speaking",
+    "Critical Analysis",
+    "Entrepreneurial Skills",
+    "Debate",
+    "Leadership Skills",
+    "Writing Skills",
   ]);
 
   function handleSubmit(event) {
@@ -215,13 +215,12 @@ function RegistrationForm() {
             id="floating_phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="floating_phone"
             className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Phone number <span className="text-red-500">*</span>
+            Phone number
           </label>
         </div>
       </div>
