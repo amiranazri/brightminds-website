ad3faa95ff4aa7790804ae047d79f271d337731b
Kathiressan Sivanes <kathiressan@lifelinelab.io>
2024-12-03 23:10:43 +0800

    remove phone pattern match in all forms

--- app/contact/GeneralForm.js
+++ app/contact/GeneralForm.js
@@ -108,7 +108,6 @@ function GeneralForm() {
         <div className="relative z-0 w-full mb-5 group">
           <input
             type="tel"
-            pattern="\+?[1-9][0-9]{7,14}"
             name="floating_phone"
             id="floating_phone"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
